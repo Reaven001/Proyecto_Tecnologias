8e866ee50157429218177c1d0d37ceb51a8d04d4
Esteban Cuervo <estebancuervos18@gmail.com>
2021-05-31 12:01:22 -0500

    Menu actualizado

--- src/components/Menu.js
+++ src/components/Menu.js
@@ -2,9 +2,10 @@ import React from "react";
 import { Link } from "react-router-dom";
 import firebase from "firebase";
 import Image from "react-bootstrap/Image";
-import { firebasedb } from "../index"
-
-
+import { firebasedb } from "../index";
+import Accordion from "react-bootstrap/Accordion";
+import Button from "react-bootstrap/Button";
+import Card from "react-bootstrap/Card";
 
 import "../css/menu.css";
 
@@ -14,7 +15,7 @@ class Nav extends React.Component {
     this.state = {
       user: null,
       useradmin: true,
-      Admos: []
+      Admos: [],
     };
 
     this.handleLogout = this.handleLogout.bind(this);
@@ -28,10 +29,7 @@ class Nav extends React.Component {
         this.setState({ Admos: [] });
       }
     });
-
-  }
-
-
+  };
 
   componentWillMount() {
     firebase.auth().onAuthStateChanged((user) => {
@@ -49,7 +47,6 @@ class Nav extends React.Component {
   }
 
   renderUserLogin() {
-
     if (this.state.user) {
       //Si el usuario esta logueado:
       return (
@@ -73,55 +70,117 @@ class Nav extends React.Component {
                   roundedCircle
                 />
               </div>
-                            {Object.keys(this.state.Admos).map(x => {
-
-              return <div key={x}>
-
-                {this.state.user.email == this.state.Admos[x].email &&
-                  <div>
-
+              {Object.keys(this.state.Admos).map((x) => {
+                return (
+                  <div key={x}>
+                    {this.state.user.email == this.state.Admos[x].email && (
+                      <div>
                         <li className="list">
-                        <Link
-                          to="/Admins"
-                          style={{ textDecoration: "none", color: "#000000" }}
-                        >
-                          Admins
-                          
-                        </Link>
-                      </li>
-
+                          <Link
+                            to="/Admins"
+                            style={{ textDecoration: "none", color: "#e1ff00" }}
+                          >
+                            Admins
+                          </Link>
+                        </li>
+                      </div>
+                    )}
                   </div>
-                }
-
-              </div>
-
-
+                );
               })}
               <li className="list">
                 <Link
                   to="/Inicio"
-                  style={{ textDecoration: "none", color: "#000000" }}
+                  style={{ textDecoration: "none", color: "#e1ff00" }}
                 >
                   Inicio
-                  
                 </Link>
               </li>
 
               <li className="list">
-                <Link
-                  to="/"
-                  style={{ textDecoration: "none", color: "#000000" }}
-                >
-                  Areas
-                </Link>
+                <Accordion>
+                  <Card
+                    style={{
+                      background: "#0d6eff",
+                      border: "0px",
+                    }}
+                  >
+                    <Card.Header
+                      style={{
+                        background: "#0d6eff",
+                      }}
+                    >
+                      <Accordion.Toggle
+                        eventKey="0"
+                        style={{
+                          border: "0px",
+                          background: "#0d6eff",
+                          color: "#e1ff00",
+                        }}
+                      >
+                        Áreas
+                      </Accordion.Toggle>
+                    </Card.Header>
+                    <Accordion.Collapse eventKey="0">
+                      <Card.Body>
+                        <ul>
+                          <li className="list">
+                            <Link
+                              to="/TransformacionDigital"
+                              style={{
+                                textDecoration: "none",
+                                color: "#e1ff00",
+                              }}
+                            >
+                              Transformación Digital
+                            </Link>
+                          </li>
+                          <li className="list">
+                            <Link
+                              to="/Procesamientodeanalisisdeinformacionmultimedia"
+                              style={{
+                                textDecoration: "none",
+                                color: "#e1ff00",
+                              }}
+                            >
+                              Procesamiento de análisis de información
+                              multimedia
+                            </Link>
+                          </li>
+
+                          <li className="list">
+                            <Link
+                              to="/Sistemasinteractivos"
+                              style={{
+                                textDecoration: "none",
+                                color: "#e1ff00",
+                              }}
+                            >
+                              Sistemas Interactvos
+                            </Link>
+                          </li>
+                          <li className="list">
+                            <Link
+                              to="/Produccionmultimedia"
+                              style={{
+                                textDecoration: "none",
+                                color: "#e1ff00",
+                              }}
+                            >
+                              Producción Multimedia
+                            </Link>
+                          </li>
+                        </ul>
+                      </Card.Body>
+                    </Accordion.Collapse>
+                  </Card>
+                </Accordion>
               </li>
-
-
 
               <li className="list">
                 <Link
                   to="/SubirArchivos"
-                  style={{ textDecoration: "none", color: "#000000" }}
+                  style={{ textDecoration: "none", color: "#e1ff00" }}
                 >
                   Subir Archivos
                 </Link>
@@ -129,7 +188,7 @@ class Nav extends React.Component {
               <li className="list" onClick={this.handleLogout}>
                 <Link
                   to="/"
-                  style={{ textDecoration: "none", color: "#000000" }}
+                  style={{ textDecoration: "none", color: "#e1ff00" }}
                 >
                   Cerrar Sesión
                 </Link>
@@ -151,23 +210,95 @@ class Nav extends React.Component {
               <li className="list">
                 <Link
                   to="/Inicio"
-                  style={{ textDecoration: "none", color: "#000000" }}
+                  style={{ textDecoration: "none", color: "#e1ff00" }}
                 >
                   Inicio
                 </Link>
               </li>
               <li className="list">
-                <Link
-                  to="/"
-                  style={{ textDecoration: "none", color: "#000000" }}
-                >
-                  Areas
-                </Link>
+                <Accordion>
+                  <Card
+                    style={{
+                      background: "#0d6eff",
+                      border: "0px",
+                    }}
+                  >
+                    <Card.Header
+                      style={{
+                        background: "#0d6eff",
+                      }}
+                    >
+                      <Accordion.Toggle
+                        eventKey="0"
+                        style={{
+                          border: "0px",
+                          background: "#0d6eff",
+                          color: "#e1ff00",
+                        }}
+                      >
+                        Áreas
+                      </Accordion.Toggle>
+                    </Card.Header>
+                    <Accordion.Collapse eventKey="0">
+                      <Card.Body>
+                        <ul>
+                          <li className="list">
+                            <Link
+                              to="/TransformacionDigital"
+                              style={{
+                                textDecoration: "none",
+                                color: "#e1ff00",
+                              }}
+                            >
+                              Transformación Digital
+                            </Link>
+                          </li>
+                          <li className="list">
+                            <Link
+                              to="/Procesamientodeanalisisdeinformacionmultimedia"
+                              style={{
+                                textDecoration: "none",
+                                color: "#e1ff00",
+                              }}
+                            >
+                              Procesamiento de análisis de información
+                              multimedia
+                            </Link>
+                          </li>
+
+                          <li className="list">
+                            <Link
+                              to="/Sistemasinteractivos"
+                              style={{
+                                textDecoration: "none",
+                                color: "#e1ff00",
+                              }}
+                            >
+                              Sistemas Interactvos
+                            </Link>
+                          </li>
+                          <li className="list">
+                            <Link
+                              to="/Produccionmultimedia"
+                              style={{
+                                textDecoration: "none",
+                                color: "#e1ff00",
+                              }}
+                            >
+                              Producción Multimedia
+                            </Link>
+                          </li>
+                        </ul>
+                      </Card.Body>
+                    </Accordion.Collapse>
+                  </Card>
+                </Accordion>
               </li>
+
               <li className="list">
                 <Link
                   to="/SubirArchivos"
-                  style={{ textDecoration: "none", color: "#000000" }}
+                  style={{ textDecoration: "none", color: "#e1ff00" }}
                 >
                   Subir Archivos
                 </Link>
@@ -175,7 +306,7 @@ class Nav extends React.Component {
               <li className="list">
                 <Link
                   to="/IniciarSesion"
-                  style={{ textDecoration: "none", color: "#000000" }}
+                  style={{ textDecoration: "none", color: "#e1ff00" }}
                 >
                   Iniciar Sesión
                 </Link>
